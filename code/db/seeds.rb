eabd83f38f59b413aac0ca03aea3e9a16e72fa5c
EclecticKlos <dsklos@gmail.com>
2015-02-05 22:02:57 -0800

    Fixed seeding

--- code/db/seeds.rb
+++ code/db/seeds.rb
@@ -1,7 +1,9 @@
 require 'faker'
 
 10.times do
-  User.new(username: Faker::Internet.user_name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  User.create(username: Faker::Internet.user_name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
 
 10.times do
-  Skill.new(skill_name: Faker::Lorem.sentence)
+  Skill.create(skill_name: Faker::Lorem.sentence)
+end
