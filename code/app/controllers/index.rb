4c69f2775a512738bb4eba3d17ea23de899de608
EclecticKlos <dsklos@gmail.com>
2015-02-08 01:14:41 -0800

    Shit is broken

--- code/app/controllers/index.rb
+++ code/app/controllers/index.rb
@@ -12,15 +12,40 @@ post '/login' do
   user = User.find_by(username: params[:username])
   if user.authenticate(params[:password])
     session[:user_id] = user.id
-    redirect "/#{user.username}"
+    redirect "/#{user.id}"
   else
     redirect "/login"
   end
 end
 
+get '/:user/skill_select' do
+  @user_id = session[:user_id]
+  @skills = (Skill.all - Skill.where(skill_name: NIL)) # TO DO......
+  erb :skill_select
+end
+
+post '/:user/skill_select' do
+  #p Skill.create(skill_name: params[:skill_name], user_id: session[:user_id])
+  #for i in 0..16 do
+  #  Skill.create(skill_name: params[i], user_id: session[:user_id])
+  #end
+
+  p
+  redirect "/#{session[:user_id]}"
+end
+
 get '/:user' do
   @user = User.find(session[:user_id])
-
+  puts "=" * 50
+  puts "User ID: #{@user.id}"
+  puts "User name: #{@user.username}"
+  puts "=" * 50
+  @skills = @user.skills
+  puts "Skill cnt: #{@skills.count}"
   erb :user
 end
 
+# get '/users/:id/skill_select/:skill_id'
+
+
+# <% User.find_by(id: session[:user_id]) %>
--- code/db/seeds.rb
+++ code/db/seeds.rb
@@ -5,12 +5,14 @@ require 'faker'
   User.create(username: temp_name, password: temp_name, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 end
 
-10.times do
-  Skill.create(skill_name: Faker::Lorem.sentence)
-end
-
 Skill.create(skill_name: "Algorithms")
 Skill.create(skill_name: "OOP Ruby")
 Skill.create(skill_name: "MVC")
 Skill.create(skill_name: "Sinatra")
 Skill.create(skill_name: "OOP JavaScript")
+
+10.times do
+  Skill.create(skill_name: Faker::Lorem.sentence)
+end
+
+
